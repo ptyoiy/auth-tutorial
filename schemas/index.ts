1e5991ad7b3783737414ff8a2e24b1ee4ccde49d
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-08-31 13:17:47 +0900

    feat: make Login form
    feat: make Login form
    
    로그인 폼 제작 완료
    shadcn의 form, input 컴포넌트 활용
    zod와 react hook form을 shadcn form에서 사용하므로 login schema도 설정
    login-form을 zod와 react hook form을 사용해서 제작

---
+++ schemas/index.ts
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+/**
+ * 신규 가입은 길이 제한을 해도 되지만
+ * 로그인 때는 pw에 길이 제한을 하지 않는 것 추천
+ * 규칙이 어떻게 바뀔지 모르고, 
+ * 제한을 하는 목적성이 없기에 굳이 필요하진 않음
+ */
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: "Email is required"
+  }),
+  password: z.string().min(1, {
+    message: "Password is required"
+  })
+})
