4eafa3deece8e768cebc76beceaaf82f9bb62846
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-09-01 10:15:28 +0900

    style: util 함수로 교체

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { getUserByEmail } from '@/data/user';
 import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 import bcrypt from 'bcrypt';
@@ -15,11 +16,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const { email, password, name } = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const existingUser = await db.user.findUnique({
-    where: {
-      email,
-    },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
     return { error: 'Email already in use!' };
